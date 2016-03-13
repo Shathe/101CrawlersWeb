e069a06352950078d15ab36ad16382be691ba908
Francisco J Lopez-Pellicer <fjlopez@unizar.es>
2016-03-13 20:52:19 +0100

    - Recodificación de CrawlPluginsValidator. No se puede referenciar a una clase de test desde una clase en main. Además se ha simplificado el código ya que cuando se introduzca Spring se utilizará http://docs.spring.io/spring/docs/current/spring-framework-reference/htmlsingle/#resources

--- butler/src/main/java/es/unizar/iaaa/crawler/butler/builders/validator/CrawlPluginsValidator.java
+++ butler/src/main/java/es/unizar/iaaa/crawler/butler/builders/validator/CrawlPluginsValidator.java
@@ -1,51 +1,46 @@
 package es.unizar.iaaa.crawler.butler.builders.validator;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import es.unizar.iaaa.crawler.butler.builders.CoordinatorTest;
 import es.unizar.iaaa.crawler.butler.yalm.Configuration;
+
+import java.io.File;
 
 public class CrawlPluginsValidator implements Validator {
 
-	@Override
-	public ValidationResult validate(Configuration config) {
-		
-		boolean correcto=true;
-		String siguientePlugin=""; String sigueinteFile="";
-		for (int i = 0; i < config.getCrawlSystem().getPlugins().size(); i++) {
-			/* Estructura: nombre (file.jar)+ */
-			String [] all=config.getCrawlSystem().getPlugins().get(i).split(" ");
-			siguientePlugin=all[0];
-			InputStream in;
-			/* Compruebas que los ficheros existen */
-	        try {
-	        	String namePlun=siguientePlugin+".xml";
-	    		for (int j = 1; j < all.length; j++) {
-	    			sigueinteFile=all[j];
-		            in = Files.newInputStream(Paths.get(CoordinatorTest.class.getResource(sigueinteFile).toURI()));
-	    		}
-	    		sigueinteFile=namePlun;
-	            in = Files.newInputStream(Paths.get(CoordinatorTest.class.getResource(sigueinteFile).toURI()));
-	            
-	        } catch (Exception e) {
-	            // TODO Auto-generated catch block
-	        	correcto=false;
-	        }
-		}
-		if (correcto) {
-			return new LatestValidationResult();
-		}
-		else {
-			return new LatestValidationResult(Validator.ErroresValidar.ERROR_UNSUPPORTED_CRAWL_PLUGINS, 
-					"Error en el plugin:"+siguientePlugin +", no se encuentra:"+sigueinteFile );
-		}
+    @Override
+    public ValidationResult validate(Configuration config) {
 
-	}
+        for (int i = 0; i < config.getCrawlSystem().getPlugins().size(); i++) {
+            /* Estructura: nombre (file.jar)+ */
+            String[] all = config.getCrawlSystem().getPlugins().get(i).split(" ");
+            String plugin = all[0];
+			/* Compruebas que los ficheros existen */
+
+            for (int j = 1; j < all.length; j++) {
+                if (!checkFileExists(all[j])) {
+                    return new LatestValidationResult(ErroresValidar.ERROR_UNSUPPORTED_CRAWL_PLUGINS,
+                            "Error en el plugin: " + plugin + ", no se encuentra: " + all[j]);
+                }
+            }
+            String namePlun = plugin + ".xml";
+            if (!checkFileExists(namePlun)) {
+                return new LatestValidationResult(ErroresValidar.ERROR_UNSUPPORTED_CRAWL_PLUGINS,
+                        "Error en el plugin: " + plugin + ", no se encuentra: " + namePlun);
+            }
+            ;
+        }
+        return new LatestValidationResult();
+    }
+
+    private boolean checkFileExists(String file) {
+        try {
+            // TODO Determinar la mejor manera de comprobar la existencia de recursos (¿Tenemos que introducir ya Spring?)
+            if (new File(file).exists()) {
+                return true;
+            }
+        } catch (Exception e) {
+            return false;
+        }
+        return false;
+    }
 
 }
--- butler/src/main/java/es/unizar/iaaa/crawler/butler/yalm/YamlConfigRunner.java
+++ butler/src/main/java/es/unizar/iaaa/crawler/butler/yalm/YamlConfigRunner.java
@@ -11,7 +11,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 
 
 public class YamlConfigRunner {
