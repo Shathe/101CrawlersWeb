c5e61b86e87022d3d329ae209c4a5698677ca7ae
Francisco J Lopez-Pellicer <fjlopez@unizar.es>
2016-03-24 19:16:33 +0100

    Mover a Application de sitio y racionaliar anotaciones

---
+++ butler/src/main/java/es/unizar/iaaa/crawler/butler/Application.java
@@ -0,0 +1,15 @@
+package es.unizar.iaaa.crawler.butler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Application.class, args);
+
+	}
+
+}
