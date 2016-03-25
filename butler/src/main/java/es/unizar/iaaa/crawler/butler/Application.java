57fec05cdf5c6729432e6455431cd668e9b36219
Iñigo Alonso Ruiz <inigol22zgz@gmail.com>
2016-03-25 15:36:25 +0100

    spring shell HelloWolrd with test

--- butler/src/main/java/es/unizar/iaaa/crawler/butler/Application.java
+++ butler/src/main/java/es/unizar/iaaa/crawler/butler/Application.java
@@ -1,14 +1,18 @@
 package es.unizar.iaaa.crawler.butler;
 
+import java.io.IOException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.shell.Bootstrap;
 
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)throws IOException {
 
-		SpringApplication.run(Application.class, args);
+				Bootstrap.main(args);
+		//SpringApplication.run(Application.class, args);
 
 	}
 
