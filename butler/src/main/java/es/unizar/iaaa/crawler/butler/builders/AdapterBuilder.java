875969d588049d533b7e92432ab8100278e1463b
Iñigo Alonso Ruiz <inigol22zgz@gmail.com>
2016-03-29 17:31:12 +0200

    No error if there were an existing folder

--- butler/src/main/java/es/unizar/iaaa/crawler/butler/builders/AdapterBuilder.java
+++ butler/src/main/java/es/unizar/iaaa/crawler/butler/builders/AdapterBuilder.java
@@ -4,12 +4,17 @@
 
 package es.unizar.iaaa.crawler.butler.builders;
 
+import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
+
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import es.unizar.iaaa.crawler.butler.model.CrawlConfiguration;
 
@@ -41,7 +46,21 @@ public class AdapterBuilder {
         // o OS's para docker, el adaptador eligiría la adecuada segun la configuarión
         File theDir = new File(outputDir);
         theDir.mkdir();
+        Path newDirectoryPath = Paths.get(outputDir);
 
+        if (Files.exists(newDirectoryPath)) {
+        	// If the folder exists, delete it
+        	// In practice, as the user and crawler are unic, it is only 
+        	// possible to happen if it's not the first time you're trying to build it
+        	FileUtils.deleteDirectory(new File(outputDir)); 
+        }
+        try {
+            Files.createDirectory(newDirectoryPath);
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+        
+        
         // First crawl builder and then docker builder
         // Primero se llama al builder de nutch y después al de docker
         builder.createNutchSite(configuration, outputDir);
