da4ec38e1c8de39c59f64a51e6d6e9740b11dd9a
Iñigo Alonso Ruiz <inigol22zgz@gmail.com>
2016-03-29 17:13:00 +0200

    validator timeouts

--- butler/src/main/java/es/unizar/iaaa/crawler/butler/validator/CrawlTimeoutsValidator.java
+++ butler/src/main/java/es/unizar/iaaa/crawler/butler/validator/CrawlTimeoutsValidator.java
@@ -4,7 +4,10 @@
 
 package es.unizar.iaaa.crawler.butler.validator;
 
+import java.util.ArrayList;
+
 import es.unizar.iaaa.crawler.butler.model.CrawlConfiguration;
+import es.unizar.iaaa.crawler.butler.validator.Validator.Status;
 
 /**
  * Validates if the Crawl timeouts is well formed in this case it's well formed
@@ -12,24 +15,26 @@ import es.unizar.iaaa.crawler.butler.model.CrawlConfiguration;
  */
 public class CrawlTimeoutsValidator implements Validator {
 
-    @Override
-    public ValidationResult validate(CrawlConfiguration config) {
-        try {
-            if (config.getCrawlSystem().getTimeouts() != null) {
-                // Validates whether they are integer numbers
-                if (config.getCrawlSystem().getTimeouts().getFetchTimes() != null)
-                    Integer.valueOf(config.getCrawlSystem().getTimeouts().getFetchTimes());
-                if (config.getCrawlSystem().getTimeouts().getNetwork() != null)
-                    Integer.valueOf(config.getCrawlSystem().getTimeouts().getNetwork());
-                if (config.getCrawlSystem().getTimeouts().getParser() != null)
-                    Integer.valueOf(config.getCrawlSystem().getTimeouts().getParser());
-            }
-            return new LatestValidationResult();
+	@Override
+	public ValidationResult validate(CrawlConfiguration config) {
+		ArrayList<ValidationResult> timeouts = new ArrayList<ValidationResult>();
+		if (config.getCrawlSystem().getTimeouts() != null) {
+			// Validates whether they are integer numbers
+			String value = config.getCrawlSystem().getTimeouts().getFetchTimes();
+			timeouts.add(validateIntValue(value, Status.ERROR_UNSUPPORTED_CRAWL_TIMEOUTS, false));
+			value = config.getCrawlSystem().getTimeouts().getNetwork();
+			timeouts.add(validateIntValue(value, Status.ERROR_UNSUPPORTED_CRAWL_TIMEOUTS, false));
+			value = config.getCrawlSystem().getTimeouts().getParser();
+			timeouts.add(validateIntValue(value, Status.ERROR_UNSUPPORTED_CRAWL_TIMEOUTS, false));
+			for (int i = 0; i < timeouts.size(); i++) {
+				// If any timeout validator Result is not ok, returns it
+				if (!timeouts.get(i).isOk())
+					return timeouts.get(i);
+			}
+		}
+		// If theres no timeouts or all of them are OK
+		return new LatestValidationResult();
 
-        } catch (Exception a) {
-            return new LatestValidationResult(Status.ERROR_UNSUPPORTED_CRAWL_TIMEOUTS,
-                    config.getCrawlSystem().getTimeouts());
-        }
-    }
+	}
 
 }
