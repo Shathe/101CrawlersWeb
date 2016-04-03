0d3c78c85ed6cca742c04342d8cff33e2ed19183
Iñigo Alonso Ruiz <inigol22zgz@gmail.com>
2016-04-04 00:38:16 +0200

    mas pruebas

--- butler/src/main/java/es/unizar/iaaa/crawler/butler/Application.java
+++ butler/src/main/java/es/unizar/iaaa/crawler/butler/Application.java
@@ -1,10 +1,14 @@
 package es.unizar.iaaa.crawler.butler;
 
+import org.apache.commons.io.FileUtils;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import es.unizar.iaaa.crawler.butler.index.DockerIndexer;
 
@@ -20,7 +24,18 @@ public class Application {
         } else if (args[0].contains("index")) {
             // The docker will be index
             DockerIndexer dockerI = new DockerIndexer();
-            dockerI.indexDocker("index", "salida/salia");
+            String indexPath="index";
+    		Path outputPath = Paths.get(indexPath);
+
+    		// If the folder exists, delete it (rewrite the index)
+    		try {
+    			FileUtils.deleteDirectory(outputPath.toFile());
+    			Files.createDirectory(outputPath);
+
+    		} catch (IOException e1) {
+    			System.out.println("Failing creating the index folder");
+    		}
+            dockerI.indexDocker(indexPath, "salida/salia");
         }
     }
 
